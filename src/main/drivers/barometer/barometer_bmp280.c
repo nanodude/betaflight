8b2b8669079cab87d893dd26aa054c5d8fcd22b7
Martin Luessi <mluessi@gmail.com>
2017-11-29 16:57:03 -0800

    BMP280: fix SPI divisor

--- src/main/drivers/barometer/barometer_bmp280.c
+++ src/main/drivers/barometer/barometer_bmp280.c
@@ -102,7 +102,7 @@ void bmp280BusInit(busDevice_t *busdev)
         IOInit(busdev->busdev_u.spi.csnPin, OWNER_BARO_CS, 0);
         IOConfigGPIO(busdev->busdev_u.spi.csnPin, IOCFG_OUT_PP);
         IOHi(busdev->busdev_u.spi.csnPin); // Disable
-        spiSetDivisor(busdev->busdev_u.spi.instance, SPI_CLOCK_STANDARD); // XXX
+        spiSetDivisor(busdev->busdev_u.spi.instance, BMP280_SPI_DIVISOR); // XXX
     }
 #else
     UNUSED(busdev);
