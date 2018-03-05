5b8ae67c8aaad2c0b317dd4bf8ce30847f13c38c
Martin <mluessi@gmail.com>
2018-03-04 16:08:49 -0800

    CLI: add chibios stack usage

--- src/main/interface/cli.c
+++ src/main/interface/cli.c
@@ -3075,6 +3075,28 @@ static void cliStatus(char *cmdline)
 }
 
 #ifndef SKIP_TASK_STATISTICS
+
+#if defined(USE_CHIBIOS)
+#include "ch.h"
+
+uint32_t ChibiGetTaskStackUsage(thread_t *threadp)
+{
+#if CH_DBG_FILL_THREADS
+    uint32_t *stack = (uint32_t*)((size_t)threadp + sizeof(*threadp));
+    uint32_t *stklimit = stack;
+    while (*stack ==
+            ((CH_DBG_STACK_FILL_VALUE << 24) |
+            (CH_DBG_STACK_FILL_VALUE << 16) |
+            (CH_DBG_STACK_FILL_VALUE << 8) |
+            (CH_DBG_STACK_FILL_VALUE << 0)))
+        ++stack;
+    return (stack - stklimit) * 4;
+#else
+    return 0;
+#endif /* CH_DBG_FILL_THREADS */
+}
+#endif /* defined(USE_CHIBIOS) */
+
 static void cliTasks(char *cmdline)
 {
     UNUSED(cmdline);
@@ -3131,6 +3153,15 @@ static void cliTasks(char *cmdline)
         cliPrintLinef("RX Check Function %19d %7d %25d", checkFuncInfo.maxExecutionTime, checkFuncInfo.averageExecutionTime, checkFuncInfo.totalExecutionTime / 1000);
         cliPrintLinef("Total (excluding SERIAL) %25d.%1d%% %4d.%1d%%", maxLoadSum/10, maxLoadSum%10, averageLoadSum/10, averageLoadSum%10);
     }
+#if defined(USE_CHIBIOS)
+    cliPrintf("\r\nChibiOS tasks remaining stacks:\r\n");
+    thread_t *threadp = chRegFirstThread();
+    while (threadp != NULL) {
+        uint32_t rem = ChibiGetTaskStackUsage(threadp);
+        cliPrintf("%s: %d Bytes free\r\n", chRegGetThreadNameX(threadp), rem);
+        threadp = chRegNextThread(threadp);
+    }
+#endif /* defined(USE_CHIBIOS) */
 }
 #endif
 
