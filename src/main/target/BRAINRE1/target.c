a290a6bcea323454707cf3cb790d4eaef5b7bc62
Martin Luessi <mluessi@gmail.com>
2018-03-05 15:23:33 -0800

    RE1: use new timer def

--- src/main/target/BRAINRE1/target.c
+++ src/main/target/BRAINRE1/target.c
@@ -34,6 +34,7 @@
 #include "drivers/light_led.h"
 #include "drivers/sound_beeper.h"
 #include "drivers/timer.h"
+#include "drivers/timer_def.h"
 #include "drivers/serial.h"
 #include "drivers/serial_softserial.h"
 #include "drivers/serial_uart.h"
@@ -91,13 +92,20 @@
 #include "brainfpv/brainfpv_osd.h"
 
 const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
-    { TIM12, IO_TAG(PB14), TIM_Channel_1, TIM_USE_PPM,   0, GPIO_AF_TIM12, NULL,  0, 0, },    // PPM_IN
-    { TIM5,  IO_TAG(PA0),  TIM_Channel_1, TIM_USE_MOTOR, 1, GPIO_AF_TIM5,  DMA1_Stream2, DMA_Channel_6, DMA1_ST2_HANDLER}, // S1_OUT
-    { TIM5,  IO_TAG(PA1),  TIM_Channel_2, TIM_USE_MOTOR, 1, GPIO_AF_TIM5,  DMA1_Stream4, DMA_Channel_6, DMA1_ST4_HANDLER}, // S2_OUT
-    { TIM5,  IO_TAG(PA2),  TIM_Channel_3, TIM_USE_MOTOR, 1, GPIO_AF_TIM5,  DMA1_Stream0, DMA_Channel_6, DMA1_ST0_HANDLER}, // S3_OUT
-    { TIM5,  IO_TAG(PA3),  TIM_Channel_4, TIM_USE_MOTOR, 1, GPIO_AF_TIM5,  DMA1_Stream1, DMA_Channel_6, DMA1_ST1_HANDLER}, // S4_OUT
-    { TIM1,  IO_TAG(PA10), TIM_Channel_3, TIM_USE_MOTOR, 1, GPIO_AF_TIM1,  DMA2_Stream6, DMA_Channel_0, DMA2_ST6_HANDLER}, // S5_OUT
-    { TIM2,  IO_TAG(PA15), TIM_Channel_1, TIM_USE_MOTOR, 1, GPIO_AF_TIM2,  DMA1_Stream5, DMA_Channel_3, DMA1_ST5_HANDLER}, // S6_OUT
+    //{ TIM12, IO_TAG(PB14), TIM_Channel_1, TIM_USE_PPM,   0, GPIO_AF_TIM12, NULL,  0, 0, },    // PPM_IN
+    //{ TIM5,  IO_TAG(PA0),  TIM_Channel_1, TIM_USE_MOTOR, 1, GPIO_AF_TIM5,  DMA1_Stream2, DMA_Channel_6, DMA1_ST2_HANDLER}, // S1_OUT
+    //{ TIM5,  IO_TAG(PA1),  TIM_Channel_2, TIM_USE_MOTOR, 1, GPIO_AF_TIM5,  DMA1_Stream4, DMA_Channel_6, DMA1_ST4_HANDLER}, // S2_OUT
+    //{ TIM5,  IO_TAG(PA2),  TIM_Channel_3, TIM_USE_MOTOR, 1, GPIO_AF_TIM5,  DMA1_Stream0, DMA_Channel_6, DMA1_ST0_HANDLER}, // S3_OUT
+    //{ TIM5,  IO_TAG(PA3),  TIM_Channel_4, TIM_USE_MOTOR, 1, GPIO_AF_TIM5,  DMA1_Stream1, DMA_Channel_6, DMA1_ST1_HANDLER}, // S4_OUT
+    //{ TIM1,  IO_TAG(PA10), TIM_Channel_3, TIM_USE_MOTOR, 1, GPIO_AF_TIM1,  DMA2_Stream6, DMA_Channel_0, DMA2_ST6_HANDLER}, // S5_OUT
+    //{ TIM2,  IO_TAG(PA15), TIM_Channel_1, TIM_USE_MOTOR, 1, GPIO_AF_TIM2,  DMA1_Stream5, DMA_Channel_3, DMA1_ST5_HANDLER}, // S6_OUT
+    DEF_TIM(TIM12, CH1, PB14, TIM_USE_PPM,   0, 0), // PPM In
+    DEF_TIM(TIM5,  CH1, PA0,  TIM_USE_MOTOR, 0, 0), // S1
+    DEF_TIM(TIM5,  CH2, PA1,  TIM_USE_MOTOR, 0, 0), // S2
+    DEF_TIM(TIM5,  CH3, PA2,  TIM_USE_MOTOR, 0, 0), // S3
+    DEF_TIM(TIM5,  CH4, PA3,  TIM_USE_MOTOR, 0, 0), // S4
+    DEF_TIM(TIM1,  CH3, PA10, TIM_USE_MOTOR, 0, 0), // S5
+    DEF_TIM(TIM2,  CH1, PA15,  TIM_USE_MOTOR, 0, 0), // S6
 };
 
 bool brainfpv_settings_updated = true;
