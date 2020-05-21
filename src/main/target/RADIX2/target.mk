0c482aac60f1a1548efd8d7879c79f7ec7b9a93a
Martin Luessi <mluessi@gmail.com>
2020-05-21 15:31:09 -0700

    Add BMI270

--- src/main/target/RADIX2/target.mk
+++ src/main/target/RADIX2/target.mk
@@ -8,5 +8,6 @@ FEATURES       += VCP ONBOARDFLASH CHIBIOS BRAINFPV SPECTROGRAPH
 TARGET_SRC += \
               drivers/accgyro/accgyro_mpu.c \
               drivers/accgyro/accgyro_spi_bmi270.c \
+              $(ROOT)/lib/main/BoschSensortec/BMI270-Sensor-API/bmi270.c \
               drivers/barometer/barometer_bmp388.c \
               
