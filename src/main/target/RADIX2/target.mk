47f6f87b8fd0caaf16a1b5919fa28fb93be456d3
Martin Luessi <mluessi@gmail.com>
2021-12-25 06:23:58 -0800

    RADIX2: Use bmi270_maximum_fifo.c

--- src/main/target/RADIX2/target.mk
+++ src/main/target/RADIX2/target.mk
@@ -9,7 +9,7 @@ FEATURES       += VCP ONBOARDFLASH CHIBIOS BRAINFPV SPECTROGRAPH BRAINFPV_BL
 TARGET_SRC += \
               drivers/accgyro/accgyro_mpu.c \
               drivers/accgyro/accgyro_spi_bmi270.c \
-              $(ROOT)/lib/main/BoschSensortec/BMI270-Sensor-API/bmi270.c \
+              $(ROOT)/lib/main/BoschSensortec/BMI270-Sensor-API/bmi270_maximum_fifo.c \
               drivers/barometer/barometer_bmp388.c \
               drivers/compass/compass_hmc5883l.c \
               drivers/compass/compass_qmc5883l.c \
