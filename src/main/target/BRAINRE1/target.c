523dee3ac9008527b972aee23670cdfe9c1c14fa
Martin Luessi <mluessi@gmail.com>
2021-12-30 07:05:32 -0800

    BRAINRE1: fix includes

--- src/main/target/BRAINRE1/target.c
+++ src/main/target/BRAINRE1/target.c
@@ -14,81 +14,20 @@
  * You should have received a copy of the GNU General Public License
  * along with Cleanflight.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include <stdbool.h>
+
 #include <stdint.h>
-#include <string.h>
-#include <math.h>
 
 #include "platform.h"
+#include "drivers/io.h"
 
-#include "common/axis.h"
-#include "common/color.h"
-#include "common/maths.h"
-#include "common/printf.h"
-
-#include "drivers/nvic.h"
-
-#include "drivers/sensor.h"
-#include "drivers/system.h"
 #include "drivers/dma.h"
-#include "drivers/light_led.h"
-#include "drivers/sound_beeper.h"
 #include "drivers/timer.h"
 #include "drivers/timer_def.h"
-#include "drivers/serial.h"
-#include "drivers/serial_softserial.h"
-#include "drivers/serial_uart.h"
-#include "drivers/pwm_output.h"
-#include "drivers/adc.h"
-#include "drivers/bus_i2c.h"
-#include "drivers/bus_spi.h"
-#include "drivers/inverter.h"
-#include "drivers/flash_m25p16.h"
-#include "drivers/sdcard.h"
-#include "drivers/usb_io.h"
-#include "drivers/transponder_ir.h"
-#include "drivers/io.h"
-#include "drivers/exti.h"
-
-#ifdef USE_BST
-#include "bus_bst.h"
-#endif
-
-#include "rx/rx.h"
-#include "rx/spektrum.h"
-
-#include "io/beeper.h"
-#include "io/serial.h"
-#include "io/flashfs.h"
-#include "io/gps.h"
-#include "io/gimbal.h"
-#include "io/ledstrip.h"
-#include "io/asyncfatfs/asyncfatfs.h"
-#include "io/transponder_ir.h"
-#include "osd/osd.h"
-
-#include "scheduler/scheduler.h"
-
-#include "sensors/sensors.h"
-#include "sensors/barometer.h"
-#include "sensors/compass.h"
-#include "sensors/acceleration.h"
-#include "sensors/gyro.h"
-#include "sensors/battery.h"
-#include "sensors/boardalignment.h"
-#include "sensors/initialisation.h"
-
-#include "telemetry/telemetry.h"
-#include "blackbox/blackbox.h"
-
-#include "flight/pid.h"
-#include "flight/imu.h"
-#include "flight/mixer.h"
-#include "flight/failsafe.h"
 
 #include "fpga_drv.h"
-#include "brainfpv/ir_transponder.h"
 #include "brainfpv/brainfpv_osd.h"
+#include "brainfpv/brainfpv_system.h"
+#include "brainfpv/ir_transponder.h"
 
 const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
     //{ TIM12, IO_TAG(PB14), TIM_Channel_1, TIM_USE_PPM,   0, GPIO_AF_TIM12, NULL,  0, 0, },    // PPM_IN
